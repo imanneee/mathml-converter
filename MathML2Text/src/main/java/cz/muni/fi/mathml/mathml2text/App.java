6b7bd2432194e7e23df51626d904780c95dbfe78
maros.kucbel <maros.kucbel@gmail.com>
2013-02-09 21:04:46 +0000

    Language input parameter from command line. Basic README file.

--- MathML2Text/src/main/java/cz/muni/fi/mathml/mathml2text/App.java
+++ MathML2Text/src/main/java/cz/muni/fi/mathml/mathml2text/App.java
@@ -1,7 +1,9 @@
 package cz.muni.fi.mathml.mathml2text;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 import org.joda.time.Duration;
 import org.joda.time.Instant;
@@ -17,19 +19,45 @@ import cz.muni.fi.mathml.mathml2text.converter.impl.XmlParserStAX;
 public class App {
     
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    /** Set of supported languages */
+    private static final Set<String> SUPPORTED_LANGUAGES;
+    
+    static {
+        SUPPORTED_LANGUAGES = new HashSet<String>(3);
+        SUPPORTED_LANGUAGES.add("en");
+        SUPPORTED_LANGUAGES.add("sk");
+        SUPPORTED_LANGUAGES.add("cs");
+    }
     
     public static void main(final String[] args) {
-        if (args.length != 1) {
+        if (args.length < 1) {
             System.out.println("Please specify input file path.");
             System.exit(1);
         }
-        File input = new File(args[0]);
+        int fileArg = 0;
+        // default language is english
+        String language = "en";
+        if (args.length > 1) {
+            // some parameters were specified, only support language option
+            if (!"-l".equals(args[0])) {
+                System.out.println("Unknow parameter [" + args[0] + "].");
+                System.exit(1);
+            }
+            language = args[1];
+            // check if given language is supported
+            if (!SUPPORTED_LANGUAGES.contains(language)) {
+                System.out.println("Unsupported language.");
+                System.exit(1);
+            }
+            fileArg = 2;
+        }
         
+        File input = new File(args[fileArg]);
         final Instant start = Instant.now();
 
         final XmlParserStAX parser = new XmlParserStAX();
 
-        File parse = parser.parse(input, new Locale("cs"));
+        File parse = parser.parse(input, new Locale(language));
         
         /**************************************************************/
         /*  You can instantiate parser from inside application and run the parse() methods.  */
